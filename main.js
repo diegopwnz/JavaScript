53e24a9e3b6cae8f37da960443359b37fb0a30f6
diegopwnz <74705394+diegopwnz@users.noreply.github.com>
2021-08-13 18:41:05 +0400

    start

---
+++ main.js
@@ -0,0 +1,46 @@
+// 1
+// let userName = prompt("ВВедите ваше имя");
+// alert(userName);
+
+
+// 2
+
+// const currentYear = 2021;
+// let age = prompt("Введите ваш год рождения");
+
+// let result = currentYear - age;
+// alert(`Вам ${result} лет`);
+
+//3
+
+// let squareLegnth = prompt("Введите длинну квадрат");
+
+// let result = squareLegnth * 4;
+// alert(`Пермиетр квадрата равен ${result}`);
+
+//4
+
+// let radius = prompt("Введите радиус окружности");
+
+// const pi = 3.14;
+
+// let result = pi * radius;
+// alert(`Площадь окружности равна ${result}`);
+
+
+//5
+
+// let distance = prompt("Введите расстояние между городами");
+// let time = prompt("За какое время хотите добраться");
+
+// let speed = distance / time;
+// alert(`Нужно двигаться со скоростью ${speed}км/ч что бы успеть вовремя `)
+
+
+//6
+
+const aud = 75;
+const eur = 95;
+
+let dollar = prompt("Сколько долларов перевести в евро?");
+let result = 
