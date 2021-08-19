cedb4658a12fee79f064f1102b2020ab2860aa81
diegopwnz <74705394+diegopwnz@users.noreply.github.com>
2021-08-19 21:41:11 +0400

    danke

--- main.js
+++ main.js
@@ -1,88 +1,169 @@
-// 1
-// let userName = prompt("ВВедите ваше имя");
-// alert(userName);
+//1
 
+// let age = Number(prompt('Введите свой возраст', 0));
+
+// if (age <= 0 || age <= 11) {
+// 	alert('Вы ребенок');
+// } else if (age <= 12 || age <= 17) {
+// 	alert('Вы подросток');
+// } else if (age <= 18 || age <= 59) {
+// 	alert('Вы взрослый');
+// } else if (age > 60) {
+// 	alert('С вас сыпеться песок');
+// };
 
 // 2
 
-// const currentYear = 2021;
-// let age = prompt("Введите ваш год рождения");
+// let num = prompt('Введите число от 0 до 9', 0);
 
-// let result = currentYear - age;
-// alert(`Вам ${result} лет`);
+
+// switch (num) {
+// 	case '0':
+// 		alert(')');
+// 		break
+
+// 	case '1':
+// 		alert('!');
+// 		break
+
+// 	case '2':
+// 		alert('@');
+// 		break
+
+// 	case '3':
+// 		alert('#');
+// 		break
+
+// 	case '4':
+// 		alert('$');
+// 		break
+
+// 	case '5':
+// 		alert('%');
+// 		break
+
+// 	case '6':
+// 		alert('^');
+// 		break
+
+// 	case '7':
+// 		alert('&');
+// 		break
+
+// 	case '8':
+// 		alert('*');
+// 		break
+
+// 	case '9':
+// 		alert('(');
+// 		break
+
+// 	default:
+// 		alert('ERROR');
+// 		break
+// };
 
 //3
 
-// let squareLegnth = prompt("Введите длинну квадрат");
 
-// let result = squareLegnth * 4;
-// alert(`Пермиетр квадрата равен ${result}`);
+// let num = +prompt('Введите трёхзначное число:', 0);
+
+// let num1 = Math.trunc(num / 100);
+// let num2 = Math.trunc(num / 10) % 10;
+// let num3 = Math.trunc(num % 10);
+
+// if (num1 == num2 || num1 == num3 || num2 == num3) {
+// 	alert('Числа повторяються');
+// } else {
+// 	alert('Числа не повторяються');
+// };
 
 //4
 
-// let radius = prompt("Введите радиус окружности");
+// let num = prompt('Введите год', 0);
 
-// const pi = 3.14;
-
-// let result = pi * radius;
-// alert(`Площадь окружности равна ${result}`);
-
+// if (num % 4 == 0 && num % 100 !== 0) {
+// 	alert('Високосный год');
+// } else {
+// 	alert('Не високосный год');
+// };
 
 //5
 
-// let distance = prompt("Введите расстояние между городами");
-// let time = prompt("За какое время хотите добраться");
+// let num = prompt('Введите пятиразрядное число', 0);
 
-// let speed = distance / time;
-// alert(`Нужно двигаться со скоростью ${speed}км/ч что бы успеть вовремя `)
-
+// if (num === num.split('').reverse().join('')) {
+// 	alert('Паллиндром');
+// } else {
+// 	alert('Не Паллиндром');
+// };
 
 //6
 
-// const eur = 0.95;
+// let amount = prompt('Введите количество RUB', 0);
+// let currency = prompt('EUR = 1, UAH = 2, AZN = 3', 0);
 
-// let dollar = prompt("Сколько долларов перевести в евро?", 0);
-// let result = dollar * eur + ' Евро';
-
-// alert(result);
-
+// switch (currency) {
+// 	case '1':
+// 		alert(amount * 0.85 + 'EUR');
+// 		break
+// 	case '2':
+// 		alert(amount * 2.5 + 'UAH');
+// 		break
+// 	case '3':
+// 		alert(amount * 43 + 'AZN');
+// };
 
 
 //7
 
-// const userFile = 820;
+// let sum = prompt('Введите суммму покупки', 0);
 
-// let flash = Number(prompt('Введите объем флешки в ГБ', 0));
+// if (sum >= 200 && sum < 300) {
+// 	alert(sum / 100 * 97 + `Руб учитывая скидку 3%`);
+// } else if (sum >= 300 && sum < 500) {
+// 	alert(sum / 100 * 95 + 'Руб учитывая скидку 5%');
+// } else if (sum >= 500) {
+// 	alert(sum / 100 * 93 + 'Руб учитывая скидку 7%');
+// };
 
-// let result = flash * 1000 / userFile;
+//8 
 
-// alert(`${parseInt(result)} файлов вместиться на вашей флешке`);
+// let circle = prompt('ВВедите длину окружности:', 0);
+// let square = prompt('Введите периметр квадрата:', 0);
 
-
-//8
-
-// let cash = Number(prompt('Введите сумму денег в кошельке', 0));
-// let price = Number(prompt('Введите стоимсть шоколадки', 0));
-
-// let result = cash / price;
-// let a = cash % result;
-
-// alert(`вы можете купить ${parseInt(result)} шоколадок, ваша сдача ${Math.ceil(a)} рубасиков`);
+// if (square <= circle / 2) {
+// 	alert('Помещаеться');
+// } else {
+// 	alert('Не помещаеться');
+// }
 
 //9
 
-// let num = Number(prompt('Введите трёхзначное число', 0));
+// let ukraina = prompt('Украина це?  1 - Россия, 2 - Европпа, 3 - Це ми!', 0);
+// let russia = prompt('Россия? 1 - matreshka 2 - vodka  3 - putin', 0);
+// let time = prompt('Какое сейчас время?  1 - Время первых 2 - 17:30  3 - Время писать код', 0);
 
-// let num1 = Math.trunc(num / 100);
-// let num2 = Math.trunc((num % 100) / 10);
+// let score = 0;
 
-// let num3 = num % Math.trunc(num / 10);
+// if (ukraina == 2){
+// 	score += 2;
+// };
 
-// let result = (num3 * 100) + (num2 * 10) + num1;
-// alert(result);
+// if (russia == 3) {
+// score +=2;
+// };
 
-//10
+// if(time == 3){
+// 	score +=2;
+// };
 
-// let num = Number(prompt('Введите целое число', 0));
+// alert(`Вы набрали ${score} балла, поздравляю!`);
 
-// num % 2 == 0 ? alert('Четное') : alert('Нечетное');
+//10 
+
+// let date = prompt('Введите дату: День-Месяц-Год ');
+// date = date.split('-');
+// let oldDate = new Date(date[2], date[1] - 1, date[0]);
+// oldDate.setDate(oldDate.getDate() + 1);
+// alert(oldDate);
