cde930cde823d21110bb55d5852070e737c12ef2
diegopwnz <74705394+diegopwnz@users.noreply.github.com>
2021-09-10 12:22:50 +0400

    нет понимания как сделать другие задания

--- main.js
+++ main.js
@@ -67,11 +67,14 @@
 
 //8 
 
-h = +prompt('часы');
-m = +prompt('минуты');
-s = +prompt('секунды');
+// h = +prompt('часы');
+// m = +prompt('минуты');
+// s = +prompt('секунды');
 
-function seconds(a, b, c) {
-	return (h * 3600) + (m * 60) + s
-}
-alert(seconds(h, m, s))
+// function seconds(a, b, c) {
+// 	return (h * 3600) + (m * 60) + s
+// }
+// alert(seconds(h, m, s))
+
+//9
+
