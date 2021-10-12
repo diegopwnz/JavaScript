f84cfa6e188b5b6afd273c780c1ddb1d26654f97
diegopwnz <74705394+diegopwnz@users.noreply.github.com>
2021-10-12 21:03:11 +0400

    1,2

--- main.js
+++ main.js
@@ -1,169 +1,57 @@
 //1
 
-// let age = Number(prompt('Введите свой возраст', 0));
 
-// if (age <= 0 || age <= 11) {
-// 	alert('Вы ребенок');
-// } else if (age <= 12 || age <= 17) {
-// 	alert('Вы подросток');
-// } else if (age <= 18 || age <= 59) {
-// 	alert('Вы взрослый');
-// } else if (age > 60) {
-// 	alert('С вас сыпеться песок');
-// };
-
-// 2
-
-// let num = prompt('Введите число от 0 до 9', 0);
-
-
-// switch (num) {
-// 	case '0':
-// 		alert(')');
-// 		break
-
-// 	case '1':
-// 		alert('!');
-// 		break
-
-// 	case '2':
-// 		alert('@');
-// 		break
-
-// 	case '3':
-// 		alert('#');
-// 		break
-
-// 	case '4':
-// 		alert('$');
-// 		break
-
-// 	case '5':
-// 		alert('%');
-// 		break
-
-// 	case '6':
-// 		alert('^');
-// 		break
-
-// 	case '7':
-// 		alert('&');
-// 		break
-
-// 	case '8':
-// 		alert('*');
-// 		break
-
-// 	case '9':
-// 		alert('(');
-// 		break
-
-// 	default:
-// 		alert('ERROR');
-// 		break
-// };
-
-//3
-
-
-// let num = +prompt('Введите трёхзначное число:', 0);
-
-// let num1 = Math.trunc(num / 100);
-// let num2 = Math.trunc(num / 10) % 10;
-// let num3 = Math.trunc(num % 10);
-
-// if (num1 == num2 || num1 == num3 || num2 == num3) {
-// 	alert('Числа повторяються');
-// } else {
-// 	alert('Числа не повторяються');
-// };
-
-//4
-
-// let num = prompt('Введите год', 0);
-
-// if (num % 4 == 0 && num % 100 !== 0) {
-// 	alert('Високосный год');
-// } else {
-// 	alert('Не високосный год');
-// };
-
-//5
-
-// let num = prompt('Введите пятиразрядное число', 0);
-
-// if (num === num.split('').reverse().join('')) {
-// 	alert('Паллиндром');
-// } else {
-// 	alert('Не Паллиндром');
-// };
-
-//6
-
-// let amount = prompt('Введите количество RUB', 0);
-// let currency = prompt('EUR = 1, UAH = 2, AZN = 3', 0);
-
-// switch (currency) {
-// 	case '1':
-// 		alert(amount * 0.85 + 'EUR');
-// 		break
-// 	case '2':
-// 		alert(amount * 2.5 + 'UAH');
-// 		break
-// 	case '3':
-// 		alert(amount * 43 + 'AZN');
-// };
-
-
-//7
-
-// let sum = prompt('Введите суммму покупки', 0);
-
-// if (sum >= 200 && sum < 300) {
-// 	alert(sum / 100 * 97 + `Руб учитывая скидку 3%`);
-// } else if (sum >= 300 && sum < 500) {
-// 	alert(sum / 100 * 95 + 'Руб учитывая скидку 5%');
-// } else if (sum >= 500) {
-// 	alert(sum / 100 * 93 + 'Руб учитывая скидку 7%');
-// };
-
-//8 
-
-// let circle = prompt('ВВедите длину окружности:', 0);
-// let square = prompt('Введите периметр квадрата:', 0);
-
-// if (square <= circle / 2) {
-// 	alert('Помещаеться');
-// } else {
-// 	alert('Не помещаеться');
+// class Ring {
+// 	constructor(r) {
+// 		this.r = r;
+// 	}
+// 	get radius() {
+// 		return this.r;
+// 	}
+// 	set radius(r) {
+// 		this.r = r;
+// 	}
+// 	get d() {
+// 		return this.r * 2;
+// 	}
+// 	area() {
+// 		return Math.PI * this.r * this.r;
+// 	}
+// 	circumference() {
+// 		return Math.PI * this.r * 2;
+// 	}
 // }
 
-//9
+// let ring = new Ring(5);
+// console.log(ring.radius);
+// console.log(ring.d);
+// console.log(ring.area().toFixed(2));
+// console.log(ring.circumference().toFixed(2));
 
-// let ukraina = prompt('Украина це?  1 - Россия, 2 - Европпа, 3 - Це ми!', 0);
-// let russia = prompt('Россия? 1 - matreshka 2 - vodka  3 - putin', 0);
-// let time = prompt('Какое сейчас время?  1 - Время первых 2 - 17:30  3 - Время писать код', 0);
+//2  Не получаеться сделать нормально
 
-// let score = 0;
+// class HtmlElement {
+// 	constructor(tag, autoClose, attributes, children, ...args) {
+// 		this.tag = tag;
+// 		this.autoClose = autoClose;
+// 		this.attributes = attributes;
+// 		this.children = children;
 
-// if (ukraina == 2){
-// 	score += 2;
-// };
+// 	}
 
-// if (russia == 3) {
-// score +=2;
-// };
+// 	getHtml() {
+// 		if (this.autoClose) {
+// 			return `<${this.tag} ${this.attributes.join("")}/>`;
+// 		}
+// 		return `<${this.tag} ${this.attributes.join("")}>
+// 		${this.children}
+// 		 </ ${this.tag}>`;
+// 	}
+// }
 
-// if(time == 3){
-// 	score +=2;
-// };
+// const div = new HtmlElement("div", true, ['name="login"'])
 
-// alert(`Вы набрали ${score} балла, поздравляю!`);
+// console.log(div.getHtml())
+// document.write(div.getHtml())
 
-//10 
-
-// let date = prompt('Введите дату: День-Месяц-Год ');
-// date = date.split('-');
-// let oldDate = new Date(date[2], date[1] - 1, date[0]);
-// oldDate.setDate(oldDate.getDate() + 1);
-// alert(oldDate);
+//3
