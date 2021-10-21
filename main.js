3d49cce20cde0a8b13b5bb8adebb3894f632c4de
diegopwnz <74705394+diegopwnz@users.noreply.github.com>
2021-10-21 15:38:31 +0400

    1

--- main.js
+++ main.js
@@ -1,169 +1,110 @@
-//1
+// function compare(a, b) {
+// 	if (a < b) {
+// 		return -1
+// 	} else if (a > b) {
+// 		return 1
+// 	} else {
+// 		return 0
+// 	}
+// }
 
-// let age = Number(prompt('Введите свой возраст', 0));
-
-// if (age <= 0 || age <= 11) {
-// 	alert('Вы ребенок');
-// } else if (age <= 12 || age <= 17) {
-// 	alert('Вы подросток');
-// } else if (age <= 18 || age <= 59) {
-// 	alert('Вы взрослый');
-// } else if (age > 60) {
-// 	alert('С вас сыпеться песок');
-// };
-
-// 2
-
-// let num = prompt('Введите число от 0 до 9', 0);
+// console.log(compare(4, 4))
 
 
-// switch (num) {
-// 	case '0':
-// 		alert(')');
-// 		break
+//2
+// function factorial(n) {
+// 	result = n;
+// 	for (let i = n - 1; i > 1; --i) {
+// 		result *= i;
+// 	}
+// 	return result;
+// }
 
-// 	case '1':
-// 		alert('!');
-// 		break
-
-// 	case '2':
-// 		alert('@');
-// 		break
-
-// 	case '3':
-// 		alert('#');
-// 		break
-
-// 	case '4':
-// 		alert('$');
-// 		break
-
-// 	case '5':
-// 		alert('%');
-// 		break
-
-// 	case '6':
-// 		alert('^');
-// 		break
-
-// 	case '7':
-// 		alert('&');
-// 		break
-
-// 	case '8':
-// 		alert('*');
-// 		break
-
-// 	case '9':
-// 		alert('(');
-// 		break
-
-// 	default:
-// 		alert('ERROR');
-// 		break
-// };
-
-//3
+// console.log(factorial(5));
 
 
-// let num = +prompt('Введите трёхзначное число:', 0);
+//3 
 
-// let num1 = Math.trunc(num / 100);
-// let num2 = Math.trunc(num / 10) % 10;
-// let num3 = Math.trunc(num % 10);
+// let foo = (a, b, c) => +('' + a + b + c)
 
-// if (num1 == num2 || num1 == num3 || num2 == num3) {
-// 	alert('Числа повторяються');
-// } else {
-// 	alert('Числа не повторяються');
-// };
+// console.log(foo(3, 5, 7))
 
 //4
 
-// let num = prompt('Введите год', 0);
+// function check(a, b = 0) {
+// 	if (b == 0) {
+// 		return a ** 2;
+// 	}
+// 	return a * b;
+// }
 
-// if (num % 4 == 0 && num % 100 !== 0) {
-// 	alert('Високосный год');
-// } else {
-// 	alert('Не високосный год');
-// };
+
+// console.log(check(4));
+// console.log(check(4, 8));
+
 
 //5
 
-// let num = prompt('Введите пятиразрядное число', 0);
+// function is_perfect(number) {
+// 	let temp = 0;
+// 	for (let i = 1; i <= number / 2; i++) {
+// 		if (number % i === 0) {
+// 			temp += i;
+// 		}
+// 	}
 
-// if (num === num.split('').reverse().join('')) {
-// 	alert('Паллиндром');
-// } else {
-// 	alert('Не Паллиндром');
-// };
+// 	if (temp === number && temp !== 0) {
+// 		console.log("Это идеальное число.");
+// 	} else {
+// 		console.log("Это не идеальное число.");
+// 	}
+// }
+// is_perfect(27);
+
 
 //6
 
-// let amount = prompt('Введите количество RUB', 0);
-// let currency = prompt('EUR = 1, UAH = 2, AZN = 3', 0);
+// function getPerfect(a, b) {
+// 	let result = [];
+// 	for (let i = a; i < b; i++) {
+// 		sum = 0;
+// 		for (let n = 1; n <= i / 2; n++) {
+// 			if (i % n === 0) sum += n;
+// 		}
+// 		if (i === sum) result.push(i);
+// 	}
+// 	return result;
+// }
 
-// switch (currency) {
-// 	case '1':
-// 		alert(amount * 0.85 + 'EUR');
-// 		break
-// 	case '2':
-// 		alert(amount * 2.5 + 'UAH');
-// 		break
-// 	case '3':
-// 		alert(amount * 43 + 'AZN');
-// };
-
+// console.log(getPerfect(1, 15000));
 
 //7
 
-// let sum = prompt('Введите суммму покупки', 0);
-
-// if (sum >= 200 && sum < 300) {
-// 	alert(sum / 100 * 97 + `Руб учитывая скидку 3%`);
-// } else if (sum >= 300 && sum < 500) {
-// 	alert(sum / 100 * 95 + 'Руб учитывая скидку 5%');
-// } else if (sum >= 500) {
-// 	alert(sum / 100 * 93 + 'Руб учитывая скидку 7%');
-// };
-
-//8 
-
-// let circle = prompt('ВВедите длину окружности:', 0);
-// let square = prompt('Введите периметр квадрата:', 0);
-
-// if (square <= circle / 2) {
-// 	alert('Помещаеться');
-// } else {
-// 	alert('Не помещаеться');
+// const setTime = h => {
+// 	return (m = 0) => {
+// 		return (s = 0) => {
+// 			return `${h}:${m}:${s}`
+// 		}
+// 	}
 // }
+
+// let time = setTime(22)(13)(45);
+// console.log(time);
+
+//8
+
+// function seconds(a, b, c) {
+// 	return (a * 3600) + (b * 60) + c
+// }
+
+// console.log(seconds(00, 01, 00))
+
 
 //9
 
-// let ukraina = prompt('Украина це?  1 - Россия, 2 - Европпа, 3 - Це ми!', 0);
-// let russia = prompt('Россия? 1 - matreshka 2 - vodka  3 - putin', 0);
-// let time = prompt('Какое сейчас время?  1 - Время первых 2 - 17:30  3 - Время писать код', 0);
 
-// let score = 0;
+// secToHHMMSS = seconds => `${Math.floor(seconds / 3600)}:${Math.floor((seconds % 3600) / 60)}:${Math.floor((seconds % 3600) % 60)}`
 
-// if (ukraina == 2){
-// 	score += 2;
-// };
+// console.log(secToHHMMSS(5687))
 
-// if (russia == 3) {
-// score +=2;
-// };
-
-// if(time == 3){
-// 	score +=2;
-// };
-
-// alert(`Вы набрали ${score} балла, поздравляю!`);
-
-//10 
-
-// let date = prompt('Введите дату: День-Месяц-Год ');
-// date = date.split('-');
-// let oldDate = new Date(date[2], date[1] - 1, date[0]);
-// oldDate.setDate(oldDate.getDate() + 1);
-// alert(oldDate);
+//10
