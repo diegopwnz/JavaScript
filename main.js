12baac52be5211444b280155c9de1357978ee36d
DenisGriazev <84458483+DenisGriazev@users.noreply.github.com>
2021-11-16 20:51:22 +0400

    xx
    x

--- main.js
+++ main.js
@@ -1,167 +1,66 @@
-//1
+// let maxSpeed = 0;
+// let dayOfWeek = 0;
 
-// function stringInfo(string) {
-// 	let str = string
-// 	const sumNumbers = str.match(/\d+/g).join('').length;
-// 	const sumLetters = str.match(/[a-z]/gi).join('').length;
-// 	console.log(sumNumbers, sumLetters);
+// let calc = 2+ (6/(1+2));
+// console.log(calc)
+
+// let num = +prompt('Введите число');
+
+// if(num = 0 || num <= 9 || num < 0){
+//     alert(true)
+// } else {
+//     alert(false)
 // }
 
-// stringInfo('dsadokwqopkdapop121312451985');
 
+// x = prompt('vvedi');
 
-//2
-
-
-// function textNumber(number) {
-// 	const dict = [
-// 		['ноль', 'один', 'два', 'три', 'четыре', 'пять', 'шесть', 'семь', 'восемь', 'девять', 'десять', '', 'две', '', 'четыр', 'пят', 'шест', 'сем', 'восем', 'девят'],
-// 		['', '', 'двадцать', 'тридцать', 'сорок', '', '', '', '', 'девяносто']
-// 	];
-// 	if (number >= 0 && number <= 10)
-// 		return dict[0][number];
-// 	if (number > 10 && number <= 19)
-// 		return (dict[0][number] || dict[0][number - 10]) + 'надцать';
-// 	if (number >= 20 && number <= 99) {
-// 		const [units = 0, tens] = [...('' + number)].reverse().map(Number);
-// 		return [
-// 			tens ? dict[1][tens] || (dict[0][tens] + 'десят') : '',
-// 			units ? dict[0][units] : ''
-// 		].join(' ');
-// 	}
+// if (x == null || x == ''){
+//     alert('pustota')
+// } else  if( x == 10){
+//     alert('vse zaebok')
+// } else{
+//     alert('putin vor')
 // }
 
-// console.log(textNumber(15))
+// let x = undefined;
+// let x = false
 
-
-//3
-
-// function replaceLetter(str) {
-// 	let result = "";
-// 	for (let c of str)
-// 		result += c === c.toUpperCase() ? c.toLowerCase() : c.toUpperCase();
-
-// 	return result;
+// if (typeof x == 'boolean'){
+//     x = 35;
 // }
 
-// console.log(replaceLetter('JavaScript'))
+// alert(x)
+
+// let x = prompt('vvedi ceny');
+// let a = 0;
+
+// if(x > 1000){
+//     x = x / 100 * 90
+//  a = x / 90 * 10
+// }
+// alert(`${x}, скидка ${a} `)
 
 
-//4
-
-// function camelize(str) {
-// 	return str
-// 		.split('-')
-// 		.map(
-
-// 			(word, index) => index == 0 ? word : word[0].toUpperCase() + word.slice(1)
-// 		)
-// 		.join('');
+// let x = confirm("yes, no");
+// if (x == true){
+//     alert('Подтверждено')
+// } else {
+//     alert('Отменено')
 // }
 
-// console.log(camelize('-webkit-transition'))
+// let x = prompt('vvedi parol');
 
-//5
-
-// function acronym(words) {
-// 	if (!words) {
-// 		return '';
-// 	}
-
-// 	let first_letter = function (x) {
-// 		if (x) {
-// 			return x[0];
-// 		} else {
-// 			return '';
-// 		}
-// 	};
-
-// 	return words.split(' ').map(first_letter).join('');
+// if (x === 'Step' || x === 'Web' || x === 'JavaScript'){
+//     alert('Подтверждено');
+// } else{
+//     alert('Отклонено')
 // }
 
-// console.log(acronym('Cascading Style Sheets'))
+let x = prompt('vvedi 4islo ot 0 do 100');
 
-
-//6 
-// function longLine(...strings) {
-// 	let n = strings.length;
-// 	let newText = "";
-// 	for (let i = 0; i < n; i++) {
-// 		let text = strings[i];
-// 		newText += text;
-// 	}
-// 	return newText;
-// }
-
-// function longLineBig() {
-// 	let n = prompt("Ввести количество строк, которое нужно добавить: ");
-// 	let strings = [];
-// 	for (let i = 1; i <= n; i++) {
-// 		let text = prompt("Ввод строки №" + i + " : ");
-// 		strings.push(text);
-// 	}
-// 	alert(longLine(...strings));
-// }
-
-// longLineBig();
-
-
-//7
-
-
-//8
-
-// function task8(url) {
-// 	let arr1;
-// 	let protocol;
-// 	let domain;
-// 	let arr2;
-// 	let way;
-// 	arr1 = url.split("://");
-
-// 	protocol = arr1[0];
-// 	arr2 = arr1[1].split("/");
-// 	domain = arr2[0];
-
-// 	arr2.shift();
-// 	way = arr2.join("/");
-
-// 	return "Информация\nпротокол: " + protocol +
-// 		"\nдомен: " + domain + "\nпуть: " + way;
-// }
-
-// console.log(task8('https://itstep.org/ua/about'))
-
-
-//9
-
-// let stringDelimiter = function (sampleInput, delimiter) {
-// 	let stringArray = [''];
-// 	let j = 0;
-
-// 	for (let i = 0; i < sampleInput.length; i++) {
-// 		if (sampleInput.charAt(i) == delimiter) {
-// 			j++;
-// 			stringArray.push('');
-// 		} else {
-// 			stringArray[j] += sampleInput.charAt(i);
-// 		}
-// 	}
-// 	return stringArray;
-// }
-
-// console.log(stringDelimiter('10/08/2020', '/'))
-
-//10
-
-// const print = (sourceString, ...args) => {
-// 	let newString = sourceString;
-
-// 	args.forEach((item, index) => {
-// 		newString = newString.replace(`%${index + 1}`, item);
-// 	});
-// 	return newString
-// }
-
-// console.log(print(('Today is % 1 % 2. % 3. % 4', 'Monday', 10,
-// 	8, 2020)))
+if (x === 0 || x <= 100 && x > 0){
+    alert('prinadlej');
+} else {
+    alert('ne prinadlej');
+}
